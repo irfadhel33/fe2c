bbc677ff3fc463b74ccd16321bba1d905731593b
Irsyad Fadhel <irsyadfadhel@MacBook-Air-Irsyad.local>
2026-09-19 14:51:28 +0300

    update home path

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
